93dd5f3ce0158c3ac47fdbf43a1103e167914598
carltlindholm <carltlindholm@gmail.com>
2024-05-15 22:52:04 +0300

    Add rtc and put everything on a timed event loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <PubSubClient.h>
+#include <PubSubClient.h>  // knolleary/PubSubClient@^2.8
+#include <ESP32Time.h> // fbiego/ESP32Time@^2.0.6
 #include "/home/ctl/untracked/wifi-cred.h"
 #include "/home/ctl/untracked/mqtt-cred.h"
 
@@ -9,6 +10,14 @@
 #define LED_RED 16
 #define LED_GREEN 17
 #define LED_BLUE 18
+
+constexpr int64_t MAX_SLEEP_MS=10000;
+constexpr int64_t MIN_SLEEP_MS=50;
+// When calculating the next event, we allow the current call to be at most this much
+// in the past. This cutoff is necessary to prevent situations where some events play
+// catchup with realtime forever (next event always going into the past desite max
+// update rate).
+constexpr int64_t MAX_BACKLOG_MS=100;
 
 void setupWifi() {
   static const char ssid[] = SSID;
@@ -59,10 +68,21 @@ void setup() {
   setupWifi();
 }
 
+int64_t UpdateLeds() {
+  static int counter = 0;
 
-void loop() {
+  digitalWrite(LED_RED, counter%3 == 0);
+  digitalWrite(LED_GREEN, counter%3 == 1);
+  digitalWrite(LED_BLUE, counter%3 == 2);
+
+  ++counter;
+  return 250L;
+}
+
+int64_t UpdateMisc() {
   static int counter = 4200;
   char message[1024];
+
   PubSubClient * mqtt = getMqttClient();
   int tankPressure = analogRead(TANK_PRESSURE);
   snprintf(message, sizeof(message), 
@@ -75,8 +95,40 @@ void loop() {
   mqtt->publish("losant/" MQTT_DEV_ID "/state", message);
   digitalWrite(PUMP_CONTROL, counter < 4300 && counter%4==1); 
   Serial.printf("State: pump %d pressure %d\n", counter%2, tankPressure);
-  digitalWrite(LED_RED, counter%3 == 0);
-  digitalWrite(LED_GREEN, counter%3 == 1);
-  digitalWrite(LED_BLUE, counter%3 == 2);
-  delay(2000);
+
+  return 2000L;
+}
+
+int64_t UpdateSerial(ESP32Time *rtc) {
+  Serial.print(rtc->getTime("%Y-%m-%d %H:%M:%S"));
+  Serial.printf(".%03d\n", rtc->getMillis());
+  return 1000L;
+}
+
+void loop() {
+  static struct {
+     int64_t leds = 0;
+     int64_t serial = 0;
+     int64_t misc = 0; 
+  } next_calls_ms;
+
+  static ESP32Time rtc(2 * 3600); // UTC+2
+
+  int64_t epoch_ms = rtc.getEpoch() * 1000L + rtc.getMillis();
+  int64_t next_epoch_ms = epoch_ms + MAX_SLEEP_MS;
+
+  auto dispatch = [&] (int64_t &next_ms, std::function<int64_t()> fn) {
+    next_ms = std::max(next_ms, epoch_ms - MAX_BACKLOG_MS);
+    if (epoch_ms > next_ms) {      
+      next_ms += fn();
+    }
+    next_epoch_ms = std::min(next_ms, next_epoch_ms);
+  };
+
+  dispatch(next_calls_ms.leds, UpdateLeds);
+  dispatch(next_calls_ms.misc, UpdateMisc);
+  dispatch(next_calls_ms.serial, std::bind(UpdateSerial, &rtc));
+
+  epoch_ms = rtc.getEpoch() * 1000L + rtc.getMillis();  
+  delay(std::max(MIN_SLEEP_MS, std::min(next_epoch_ms - epoch_ms, MAX_SLEEP_MS)));
 }
