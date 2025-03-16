3db8668983a63d787f1eabcfb26298c1bed3e9d8
Carl L <carltlindholm@gmail.com>
2025-03-16 16:32:39 +0000

    Vite + Preact skeleton for setup ui.

---
+++ setup-ui/README.md
@@ -0,0 +1,16 @@
+== Project config notes
+
+```
+npm create vite@latest my-preact-app --template preact-ts
+npm create vite@latest setup-ui --template preact-ts
+cd setup-ui/
+npm install
+cp ~/my-react-app/vite.config.ts  .
+npm install vite --save-dev
+npm install vite-plugin-singlefile --save-dev
+```
+
+Develeopment
+
+npm run dev
+
---
+++ setup-ui/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [preact(), viteSingleFile()],
+  build: {
+    assetsInlineLimit: 100000000, // Inline everything
+    chunkSizeWarningLimit: 100000000, // Large enough to avoid chunking warnings
+  },
+})
